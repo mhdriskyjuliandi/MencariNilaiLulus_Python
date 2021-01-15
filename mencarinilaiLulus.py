9d5909e0bc90fcdf3feaade7e666902f7b8457e8
mhdriskyjuliandi <mhd.risky.juliandi02@gmail.com>
2021-01-15 19:03:52 +0700

    Latihan

---
+++ mencarinilaiLulus.py
@@ -0,0 +1,11 @@
+print ("++++++++++++++++++++++++++")
+nilai = int(input("Masukkan Nilai :"))
+print ("============================")
+
+if nilai>70:
+    print("Nilai Anda : ",nilai)
+    print("Keterangan : Lulus")
+else:
+    print("Nilai Anda :",nilai)
+    print("Keterangan : Tidak Lulus")
+print(20*"+")
